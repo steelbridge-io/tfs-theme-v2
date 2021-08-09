5162bd340be9b8cedd52f6f8656ab6a3522e853a
steelbridge-io <chris@steelbridge.io>
2021-08-09 08:33:10 -0700

    Removed Hat Creek Ranch

--- page-templates/stream-report-template.php
+++ page-templates/stream-report-template.php
@@ -2064,7 +2064,7 @@ get_header();
                     </div>
                     
                     <!-- ====== HAT CREEK RANCH ====== -->
-                    <div class="panel panel-default">
+                   <!-- <div class="panel panel-default">
                       
                       <div class="panel-heading accordion-toggle collapsed" role="tab" id="headingTwentyone" data-toggle="collapse" data-parent="#accordion-private" href="#collapseTwentyone" aria-expanded="true" aria-controls="collapseTwentyone">
                         <div class="row">
@@ -2076,19 +2076,19 @@ get_header();
                           <div class="col-sm-6">
                             
                             
-                            <!-- === BEGIN EDIT SECTION: REPLACE 'label-default' with 'label-danger' to select any given rating === -->
-                            <?php $hatcreekranch_closed_checkbox = get_post_meta(get_the_ID(), 'hatcreekranch-closed-checkbox', true);
-                            if(get_post_meta(get_the_ID(), 'hatcreekranch-closed-checkbox', true) == '-danger'):?>
-                              <span class="label label-default<?php echo $hatcreekranch_closed_checkbox;?>"><?php echo $hatcreekranch_closed_message; ?></span>
-                            <?php else: ?>
-                              <span class="label label-default<?php echo $hatcreekranch_checkbox_poor; ?>">Poor</span>
-                              <span class="label label-default<?php echo $hatcreekranch_checkbox_fair; ?>">Fair</span>
-                              <span class="label label-default<?php echo $hatcreekranch_checkbox_fairgood; ?>">Fair to Good</span>
-                              <span class="label label-default<?php echo $hatcreekranch_checkbox_good; ?>">Good</span>
-                              <span class="label label-default<?php echo $hatcreekranch_checkbox_great; ?>">Great</span>
-                            <?php endif; ?>
+                             === BEGIN EDIT SECTION: REPLACE 'label-default' with 'label-danger' to select any given rating ===
+                             $hatcreekranch_closed_checkbox = get_post_meta(get_the_ID(), 'hatcreekranch-closed-checkbox', true);
+                            if(get_post_meta(get_the_ID(), 'hatcreekranch-closed-checkbox', true) == '-danger'):
+                              <span class="label label-default echo $hatcreekranch_closed_checkbox;"> echo $hatcreekranch_closed_message; </span>
+                             else:
+                              <span class="label label-default echo $hatcreekranch_checkbox_poor; ">Poor</span>
+                              <span class="label label-default echo $hatcreekranch_checkbox_fair; ">Fair</span>
+                              <span class="label label-default echo $hatcreekranch_checkbox_fairgood; ">Fair to Good</span>
+                              <span class="label label-default echo $hatcreekranch_checkbox_good; ">Good</span>
+                              <span class="label label-default echo $hatcreekranch_checkbox_great; ">Great</span>
+                             endif;
                             
-                            <!-- === END EDIT SECTION === -->
+                             === END EDIT SECTION ===
                           
                           
                           </div>
@@ -2110,11 +2110,11 @@ get_header();
                                   <h4><strong>Updated:&nbsp;</strong>
                                     
                                     
-                                    <!-- === BEGIN EDIT SECTION: DATE === -->
+                                     === BEGIN EDIT SECTION: DATE ===
                                     
-                                    <?php echo $hatcreekranch_updated; ?>
+                                     echo $hatcreekranch_updated;
                                     
-                                    <!-- === END EDIT SECTION: DATE === -->
+                                     === END EDIT SECTION: DATE ===
                                   
                                   </h4>
                                 </div>
@@ -2124,11 +2124,11 @@ get_header();
                                   <p><strong>Report:</strong>
                                     
                                     
-                                    <!-- === BEGIN EDIT SECTION: REPORT === -->
+                                     === BEGIN EDIT SECTION: REPORT ===
                                     
-                                    <?php echo $hatcreekranch_report; ?>
+                                     echo $hatcreekranch_report;
                                     
-                                    <!-- === END EDIT SECTION: REPORT === -->
+                                     === END EDIT SECTION: REPORT ===
                                   
                                   
                                   </p>
@@ -2136,11 +2136,11 @@ get_header();
                                   <p><strong>Hot Flies:&nbsp;</strong>
                                     
                                     
-                                    <!-- === BEGIN EDIT SECTION: HOT FLIES === -->
+                                     === BEGIN EDIT SECTION: HOT FLIES ===
                                     
-                                    <?php echo $hatcreekranch_hot_flies; ?>
+                                     echo $hatcreekranch_hot_flies;
                                     
-                                    <!-- === END EDIT SECTION: HOT FLIES === -->
+                                     === END EDIT SECTION: HOT FLIES ===
                                   
                                   </p>
                                 </div>
@@ -2149,7 +2149,7 @@ get_header();
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div> -->
                     
                     <!-- ====== LUK LAKE ====== -->
                     <div class="panel panel-default">
